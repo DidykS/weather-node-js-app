7ba6ad6ae4852c2221c47563aec02e773b35eb7a
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-12-18 13:21:57 +0200

    start server

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 // MODULES
 // import express
 const express = require('express')
+// import dotenv
+require('dotenv').config()
+// import morgan
+const morgan = require('morgan')
 
 const app = express()
 
 // MIDDLEWARES
+// morgan logger middleware
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+// LISTEN
+// start server
+app.listen(process.env.PORT || 3000, (error) => {
+  error ? console.log(error) : console.log(`Listening port ${process.env.PORT}`)
+})
